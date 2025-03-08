c02fb66e82712c01b9ad987ab884ac7446f8d082
Elorucov <elorucov@live.ru>
2025-03-08 21:55:09 +0400

    Laney v1.26.193

--- laney/scripts/script.js
+++
@@ -1,3 +0,0 @@
-var colorSchemeQueryList=window.matchMedia('(prefers-color-scheme: dark)');colorSchemeQueryList.onchange=function(z){changeTheme(z.matches)}
-document.addEventListener("DOMContentLoaded",function(){changeTheme(colorSchemeQueryList.matches)});function changeTheme(dark){if(dark){document.documentElement.setAttribute("theme","dark")}else{if(document.documentElement.hasAttribute("theme")){document.documentElement.removeAttribute("theme")}}}
-function toggleTheme(){changeTheme(!document.documentElement.hasAttribute("theme"))}
